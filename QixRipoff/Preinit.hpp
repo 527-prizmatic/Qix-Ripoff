e2b19db33c5d0d697f234bf0ccf633fc0b98e86d
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2024-11-01 15:34:50 +0100

    qhar

--- QixRipoff/Preinit.hpp
+++ QixRipoff/Preinit.hpp
@@ -1,7 +1,11 @@
 #pragma once
+#include <string>
 
 class Core;
 namespace ctrl { class Keyboard; }
+
+extern const std::string KeyOpenMenu;
+extern const std::string KeyOpenGame;
 
 void preinit(Core& _core);
 void registerControls(Core& _core);
