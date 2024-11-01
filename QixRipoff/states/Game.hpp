f54455642405092f02bb1d8395c581196aba9925
MaxenceM <maxence.maurel0@gmail.com>
2024-11-01 17:06:24 +0100

    player base

--- QixRipoff/states/Game.hpp
+++ QixRipoff/states/Game.hpp
@@ -2,12 +2,14 @@
 #include "../toolbox/toolbox.hpp"
 #include "State.hpp"
 #include "../GameField.hpp"
+#include "../Player.hpp"
 
 namespace states {
 	class Game : public State {
 	private:
 		Core* core;
 		GameField field;
+		Player player;
 	public:
 		Game(Core* _core);
 		void init();
