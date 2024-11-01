bcde3be1d524580c7c13c9fae9b7755c79e4467c
MaxenceM <maxence.maurel0@gmail.com>
2024-11-01 14:03:33 +0100

    menu select done

--- QixRipoff/Preinit.cpp
+++ QixRipoff/Preinit.cpp
@@ -6,6 +6,10 @@
 
 const std::string KeyOpenMenu = "OpenMenu";
 const std::string KeyOpenGame = "OpenGame";
+const std::string KeySwitchSelectMenuUp = "ChoiseMenuUp";
+const std::string KeySwitchSelectMenuDown = "ChoiseMenuDown";
+const std::string KeySelectMenu = "SelectMenu";
+
 
 void preinit(Core& _core) {
 	_core.registerState(MENU, (states::State*)(new states::Menu(&_core)));
@@ -15,4 +19,7 @@ void preinit(Core& _core) {
 void registerControls(Core& _core) {
 	_core.getKeyboard().addKey(KeyOpenMenu, sf::Keyboard::Key::B);
 	_core.getKeyboard().addKey(KeyOpenGame, sf::Keyboard::Key::A);
+	_core.getKeyboard().addKey(KeySwitchSelectMenuUp, sf::Keyboard::Key::Up);
+	_core.getKeyboard().addKey(KeySwitchSelectMenuDown, sf::Keyboard::Key::Down);
+	_core.getKeyboard().addKey(KeySelectMenu, sf::Keyboard::Key::Enter);
 }
--- QixRipoff/states/Menu.cpp
+++ QixRipoff/states/Menu.cpp
@@ -6,6 +6,8 @@ sf::Texture texture;
 sf::Font font;
 sf::Text text;
 
+int choix;
+
 namespace states {
 	Menu::Menu(Core* _core) {
 		this->core = _core;
@@ -18,10 +20,33 @@ namespace states {
 		texture.loadFromFile("../assets/Textures/bgMenu.png");
 		treatPinkPixelTexture(texture);
 		sprite.setTexture(texture);
+
+		choix = 0;
 	}
 
 	void Menu::update() {
-		if (this->core->getKeyboard().pressed("OpenGame") || this->core->getMouse().clicked(sf::Mouse::Left)) this->core->requestStateChange(GameState::GAME);
+		if (this->core->getKeyboard().pressed("ChoiseMenuUp")) 
+		{
+			choix--;
+		}
+		if (this->core->getKeyboard().pressed("ChoiseMenuDown"))
+		{
+			choix++;
+		}
+		if (choix > 2) choix = 0;
+		if (choix < 0) choix = 2;
+		if (this->core->getKeyboard().pressed("SelectMenu"))
+		{
+			switch (choix) {
+			case 0:
+				this->core->requestStateChange(GameState::GAME);
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			}
+		}
 	}
 
 	void Menu::render() {
@@ -31,19 +56,19 @@ namespace states {
 
 		text.setString("Play");
 		text.setCharacterSize(15);
-		text.setFillColor(sf::Color::White);
+		text.setFillColor(choix == 0 ? sf::Color::Red : sf::Color::White);
 		text.setPosition(100, 100);
 		this->core->getWindow().draw(text);
 
 		text.setString("Options");
 		text.setCharacterSize(5);
-		text.setFillColor(sf::Color::White);
+		text.setFillColor(choix == 1 ? sf::Color::Red : sf::Color::White);
 		text.setPosition(100, 130);
 		this->core->getWindow().draw(text);
 
 		text.setString("Hight socre");
 		text.setCharacterSize(5);
-		text.setFillColor(sf::Color::White);
+		text.setFillColor(choix == 2 ? sf::Color::Red : sf::Color::White);
 		text.setPosition(100, 160);
 		this->core->getWindow().draw(text);
 	}
